3283a2c19aaa41055fe2d737e7b2e9b1b5ae5a5d
Emmanuel <348391+malweka@users.noreply.github.com>
2016-04-27 00:40:52 -0400

    cleaning up

---
+++ src/Goliath.Utilities.Web/Models/ExternalLoginModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Goliath.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ExternalLoginModel
+    {
+        /// <summary>
+        /// Gets or sets the return URL.
+        /// </summary>
+        /// <value>
+        /// The return URL.
+        /// </value>
+        public string ReturnUrl { get; set; }
+        /// <summary>
+        /// Gets or sets the provider.
+        /// </summary>
+        /// <value>
+        /// The provider.
+        /// </value>
+        public string Provider { get; set; }
+    }
+}

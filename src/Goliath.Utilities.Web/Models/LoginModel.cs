4b9fab39b78c1526125253f4c8106bfe459087c7
Emmanuel <348391+malweka@users.noreply.github.com>
2016-04-27 00:40:36 -0400

    cleaning up

--- src/Goliath.Utilities.Web/Models/LoginModel.cs
+++ src/Goliath.Utilities.Web/Models/LoginModel.cs
@@ -1,18 +1,45 @@
 ﻿namespace Goliath.Models
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class LoginModel
     {
+        /// <summary>
+        /// Gets or sets the username.
+        /// </summary>
+        /// <value>
+        /// The username.
+        /// </value>
         public string Username { get; set; }
+        /// <summary>
+        /// Gets or sets the password.
+        /// </summary>
+        /// <value>
+        /// The password.
+        /// </value>
         public string Password { get; set; }
+        /// <summary>
+        /// Gets or sets the remember me.
+        /// </summary>
+        /// <value>
+        /// The remember me.
+        /// </value>
         public bool? RememberMe { get; set; }
+        /// <summary>
+        /// Gets or sets the return URL.
+        /// </summary>
+        /// <value>
+        /// The return URL.
+        /// </value>
         public string ReturnUrl { get; set; }
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance has error.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this instance has error; otherwise, <c>false</c>.
+        /// </value>
         public bool HasError { get; set; }
         public string ErrorMessage { get; set; }
-    }
-
-    public class ExternalLoginModel
-    {
-        public string ReturnUrl { get; set; }
-        public string Provider { get; set; }
     }
 }

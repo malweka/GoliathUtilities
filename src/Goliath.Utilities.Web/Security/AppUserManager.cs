0edcdc9a43b0f5a4b45dbfb78e9dbd212b43ab2e
malweka <malweka@localhost>
2016-06-07 02:04:18 -0400

    cache

--- src/Goliath.Utilities.Web/Security/AppUserManager.cs
+++ src/Goliath.Utilities.Web/Security/AppUserManager.cs
@@ -1,5 +1,8 @@
-﻿using System.Security.Claims;
+﻿using System;
+using System.Security;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Goliath.Web.Authorization;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
@@ -7,53 +10,103 @@ using Microsoft.Owin.Security.DataProtection;
 
 namespace Goliath.Security
 {
-    ///// <summary>
-    ///// 
-    ///// </summary>
-    //public class SchedulerUserManager : UserManager<UserSession, long>
-    //{
-    //    public SchedulerUserManager(IUserStore<UserSession, long> store, IDataProtector protector) : base(store)
-    //    {
-    //        this.UserTokenProvider = new DataProtectorTokenProvider<UserSession, long>(protector);
-    //    }
+    /// <summary>
+    /// 
+    /// </summary>
+    public class GoliathUserManager : UserManager<UserSession, long>
+    {
+        public GoliathUserManager(IUserStore<UserSession, long> store, IDataProtector protector) : base(store)
+        {
+            this.UserTokenProvider = new DataProtectorTokenProvider<UserSession, long>(protector);
+        }
+    }
 
-    //    public override Task<bool> CheckPasswordAsync(UserSession user, string password)
-    //    {
-    //        return base.CheckPasswordAsync(user, password);
-    //    }
-    //}
+    /// <summary>
+    /// 
+    /// </summary>
+    public class GoliathSignInManager : SignInManager<UserSession, long>
+    {
+        public GoliathSignInManager(UserManager<UserSession, long> userManager, IAuthenticationManager authenticationManager) : base(userManager, authenticationManager)
+        {
+        }
 
-    ///// <summary>
-    ///// 
-    ///// </summary>
-    //public class SchedulerSignInManager : SignInManager<UserSession, long>
-    //{
-    //    public SchedulerSignInManager(UserManager<UserSession, long> userManager, IAuthenticationManager authenticationManager) : base(userManager, authenticationManager)
-    //    {
-    //    }
+        public override Task<ClaimsIdentity> CreateUserIdentityAsync(UserSession user)
+        {
+            var task = new Task<ClaimsIdentity>(() => user.CreateClaimsIdentity(DefaultAuthenticationTypes.ApplicationCookie));
+            return task;
+        }
+    }
 
-    //    public override Task<ClaimsIdentity> CreateUserIdentityAsync(UserSession user)
-    //    {
-    //        var task = new Task<ClaimsIdentity>(() => user.CreateClaimsIdentity(DefaultAuthenticationTypes.ApplicationCookie));
-    //        return task;
-    //    }
-    //}
+    public class IdentityUserStore : IUserStore<UserSession, long>
+    {
+        private readonly IGoliathUserAccessManager accessManager;
 
-    //public class EmailService : IIdentityMessageService
-    //{
-    //    public Task SendAsync(IdentityMessage message)
-    //    {
-    //        // Plug in your email service here to send an email.
-    //        return Task.FromResult(0);
-    //    }
-    //}
+        public IdentityUserStore(IGoliathUserAccessManager accessManager)
+        {
+            this.accessManager = accessManager;
+        }
 
-    //public class SmsService : IIdentityMessageService
-    //{
-    //    public Task SendAsync(IdentityMessage message)
-    //    {
-    //        // Plug in your SMS service here to send a text message.
-    //        return Task.FromResult(0);
-    //    }
-    //}
+        public void Dispose()
+        {
+        }
+
+        public Task CreateAsync(UserSession user)
+        {
+            return Task.FromResult(0);
+        }
+
+        public Task UpdateAsync(UserSession user)
+        {
+            return Task.FromResult(0);
+        }
+
+        public Task DeleteAsync(UserSession user)
+        {
+            return Task.FromResult(0);
+        }
+
+        /// <summary>
+        /// Finds the by identifier asynchronous.
+        /// </summary>
+        /// <param name="userId">The user identifier.</param>
+        /// <returns></returns>
+        public Task<UserSession> FindByIdAsync(long userId)
+        {
+            Task<UserSession> task = new Task<UserSession>(() =>
+            {
+                try
+                {
+                    return accessManager.FindUser(userId);
+                }
+                catch (Exception ex)
+                {
+                    throw new SecurityException("Error while trying to find user by ID.", ex);
+                }
+            });
+
+            return task;
+        }
+
+        /// <summary>
+        /// Finds the by name asynchronous.
+        /// </summary>
+        /// <param name="userName">Name of the user.</param>
+        /// <returns></returns>
+        public Task<UserSession> FindByNameAsync(string userName)
+        {
+            Task<UserSession> task = new Task<UserSession>(() =>
+            {
+                try
+                {
+                    return accessManager.FindUser(userName);
+                }
+                catch (Exception ex)
+                {
+                    throw new SecurityException("Error while trying to find user by name.", ex);
+                }
+            });
+
+            return task;
+        }
+    }
 }

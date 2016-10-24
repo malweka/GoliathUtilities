e4580edb7fc614a0e5180a162bb99a6b4713e874
Emmanuel <348391+malweka@users.noreply.github.com>
2016-10-24 17:00:18 -0400

    custom MSBuild tasks

---
+++ src/GoliathBuildTasks/AssemblyInfo.Shared.cs
@@ -0,0 +1,10 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyCompany("Oracle Corporation")]
+[assembly: AssemblyProduct("Oracle IRT Build and Deploy")]
+[assembly: AssemblyCopyright("Copyright © Oracle Corporation 2013")]
+[assembly: AssemblyTrademark("Oracle Corporation")]
+[assembly: AssemblyVersion("2.0.1.6")]
+[assembly: AssemblyFileVersion("2.0.1.6")]
+[assembly: InternalsVisibleTo("Irt.Core.Tests")]

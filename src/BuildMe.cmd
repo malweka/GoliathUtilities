1d015886f4cb8a8738498a7147a6be720bf39c6f
Emmanuel <348391+malweka@users.noreply.github.com>
2017-01-10 12:01:34 -0500

    build script

--- src/BuildMe.cmd
+++ src/BuildMe.cmd
@@ -1,3 +1,3 @@
 @echo off
-"%ProgramFiles(x86)%\MSBuild\14.0\Bin\msbuild" GoliathUtils.build %*
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\msbuild" GoliathUtils.build /p:NuGetPath="C:\Tools\Nuget" /p:BuildTools="C:\Tools\BuildTools" %*
 pause

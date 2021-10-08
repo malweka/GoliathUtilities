58f33f6f52e1ff0bcd03455d6a46ded5c07cccee
Emmanuel Hameyie <hamsman@gmail.com>
2021-10-08 14:58:08 -0400

    update build script

--- src/BuildMe.cmd
+++ src/BuildMe.cmd
@@ -1,3 +1,12 @@
 @echo off
-"%ProgramFiles(x86)%\MSBuild\14.0\Bin\msbuild" GoliathUtils.build /p:NuGetPath="C:\Tools\Nuget" /p:BuildTools="C:\Tools\BuildTools" %*
+set version=%1
+set build=%2
+set apiKey=%3
+if [%version%]==[] set version="2.1.0"
+if [%build%]==[] set build="1"
+dotnet build Goliath.Utilities/Goliath.Utilities.csproj --configuration Release -p:PackageVersion=%version%.%build% -p:Version=%version%.%build% -p:AssemblyVersion=%version%.%build% -p:FileVersion=%version%.%build%
+dotnet pack Goliath.Utilities/Goliath.Utilities.csproj --configuration Release -p:PackageVersion=%version%.%build% -p:Version=%version%.%build% -p:AssemblyVersion=%version%.%build% -p:FileVersion=%version%.%build%
+
+pause
+dotnet nuget push --api-key %apiKey% --source "github" Goliath.Utilities\bin\Release\Goliath.Utilities.%version%.%build%.nupkg
 pause

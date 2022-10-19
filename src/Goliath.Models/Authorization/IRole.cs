8e0e974dbe00d293d6e0bc4fc465d011c8b6af29
Emmanuel Hameyie <hamsman@gmail.com>
2022-10-18 21:04:50 -0400

    modularization: create models project

---
+++ src/Goliath.Models/Authorization/IRole.cs
@@ -0,0 +1,16 @@
+﻿namespace Goliath.Authorization
+{
+    public interface IRole
+    {
+
+        /// <summary>
+        /// Gets or sets the number.
+        /// </summary>
+        /// <value>
+        /// The number.
+        /// </value>
+        long RoleNumber { get; }
+        string Name { get; }
+        bool IsAdminRole { get; }
+    }
+}
---
+++ src/Goliath.Models/Enums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Goliath.Models
+{
+    [Flags]
+    public enum AuthenticationStatus
+    {
+        None = 0,
+        Authenticated = 1,
+        Denied = 2,
+        PasswordExpired = 4,
+        Disabled = 8,
+        LockedAccount = 16,
+        InvalidAccount = 32
+    }
+}
